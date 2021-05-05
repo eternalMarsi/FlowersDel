f686c10bbff00e9a073d51bac15f2956f15b7bb0
Andrew Babanin <39223191+AndreReise@users.noreply.github.com>
2021-05-05 15:15:43 +0300

    dev: shop data retrieving logic implementation

---
+++ CatalogServiceAP/Models/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogServiceAPI.Models
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ CatalogServiceAP/Models/Shop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogServiceAPI.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Rate { get; set; }
+        public List<ShopImage> Images { get; set; }
+        public List<ProductCategory> Categories { get; set; }
+    }
+}
